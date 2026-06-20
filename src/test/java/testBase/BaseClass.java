6da2e0abef9fd7e396743c8edd39da9cde320f66
Sameer <sameeridea7@gmail.com>
2026-06-20 15:36:50 +0530

    Refactor code structure for improved readability and maintainability

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,11 +1,14 @@
 package testBase;
 
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -13,6 +16,7 @@ import org.testng.annotations.Parameters;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -30,15 +34,44 @@ public class BaseClass {
         FileReader fileReader = new FileReader("./src//test//resouces//config.properties");
         properties = new Properties();
         properties.load(fileReader);
+        DesiredCapabilities cap = new DesiredCapabilities();
 
+        if(properties.getProperty("execution_env").equalsIgnoreCase("remote")){
 
-        switch (browser.toLowerCase()){
-            case "chrome" :  driver = new ChromeDriver(); break;
-            case "edge" : driver = new EdgeDriver(); break;
-            case "firefox": driver = new FirefoxDriver();break;
-            default:
-                System.out.println("Invalid browser");return;
+            //os
+            if(os.equalsIgnoreCase("windows")){
+                cap.setPlatform(Platform.WIN11);
+            } else if (os.equalsIgnoreCase("mac")) {
+                cap.setPlatform(Platform.MAC);
+            }else {
+                System.out.println("No Matching found regarding OS");
+                return;
+            }
+
+            //browser
+            switch (browser.toLowerCase()){
+                case "chrome":cap.setBrowserName("chrome");break;
+                case "edge":cap.setBrowserName("MicrosoftEdge");break;
+                default:
+                    System.out.println("No Match found");
+                    return;
+
+            }
+
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
         }
+
+
+        if(properties.getProperty("execution_env").equalsIgnoreCase("local")){
+            switch (browser.toLowerCase()){
+                case "chrome" :  driver = new ChromeDriver(); break;
+                case "edge" : driver = new EdgeDriver(); break;
+                case "firefox": driver = new FirefoxDriver();break;
+                default:
+                    System.out.println("Invalid browser");return;
+            }
+        }
+
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
